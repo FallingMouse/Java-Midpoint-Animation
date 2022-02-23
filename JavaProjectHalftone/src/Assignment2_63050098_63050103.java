8deb7b37388b50cacdddfb29c10282a07e0240bc
Kaewphet Nuruang <66639008+nobiofla@users.noreply.github.com>
2022-02-24 00:57:12 +0700

    Update Ver 2.4.0 Beta
    Complete Face Time Halftone Animation
    - Reflector code
    - Everything will run on Assignment2_xxx_xxx.java

--- JavaProjectHalftone/src/Assignment2_63050098_63050103.java
+++ JavaProjectHalftone/src/Assignment2_63050098_63050103.java
@@ -1,85 +1,198 @@
+import java.util.Scanner;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.util.*;
 import javax.swing.*;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-class Assignment2_63050098_63050103 {
-    private JFrame frame;
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Assignment2_63050098_63050103()::createAndShowGui); //We place our program on the EDT
-    }
+public class Assignment2_63050098_63050103 extends JPanel implements Runnable {
+    static Scanner fileInFace, fileInClockNumber;
     
-    private void createAndShowGui() {
-        frame = new JFrame(getClass().getSimpleName());
-        
-        CirclePane circle = new CirclePane(5); //We want to create 5 circles, we may want to add more so we change it to 10, or whatever number we want
-        
-        frame.add(circle);
-        frame.setTitle("Face From Young to Old");
-        frame.setSize(600, 600);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    double halftime = 0.0f;
+    double rotate1 = 0.0f, rotate2 = 0.0f;
+    
+    String fileInPathPerFrame;
+
+    public static void main(String[] args) throws IOException {
+		Assignment2_63050098_63050103 m = new Assignment2_63050098_63050103();
+
+		JFrame frame = new JFrame();
+		frame.add(m);
+		frame.setTitle("Assignment2_63050098_63050103");
+		frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
-        (new Thread(circle)).start();
-    } 
-    
-    //Data class
-    class Circle {
-        private Point coords;
-        private int size;
-        
-        public Circle(Point coords, int size) {
-            this.coords = coords;
-            this.size = size;
-        }
-        
-        public Point getCoords() {
-            return coords;
-        }
-        public void setCoords(Point coords) {
-            this.coords = coords;
-        }
-        public int getSize() {
-            return size;
-        }
-        public void setSize(int size) {
-            this.size = size;
+        (new Thread(m)).start();
+	}
+    public void run() {
+        int numberFileIn = 0;
+        double currentTime, elapsedTime, keyFrameTime = 0;
+        double lastTime = System.currentTimeMillis();
+
+        while(true) {
+            //control time
+            currentTime = System.currentTimeMillis();
+            elapsedTime = currentTime - lastTime;
+            lastTime = currentTime;
+            
+            //update
+            rotate1 += 0.1 * elapsedTime / 1000.0;
+            rotate2 += 1.127 * elapsedTime / 1000.0;
+
+            keyFrameTime += elapsedTime / 1000.0;
+            
+            if(keyFrameTime == 0.0f) {
+                fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage" + numberFileIn + ".txt";
+                halftime += 0.625f;
+                numberFileIn++;
+                System.out.printf("Assignment2 ver %.1f\n", keyFrameTime);
+            } else if(keyFrameTime >= halftime) {
+                if(numberFileIn <= 40) {
+                    fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage" + numberFileIn + ".txt";
+                    halftime += 0.625f;
+                    numberFileIn++;
+                } else {
+                    fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage40.txt";
+                    
+                    //Stop Thread
+                    System.out.printf("Za Warudo!!!\nThread Killed!\n");
+                    return;
+                }
+            }
+
+            //display
+            repaint();
         }
     }
-    
-    //The drawing class
-    class CirclePane extends JPanel implements Runnable {
-        private int numberOfCircles;
-        private Circle[] circles;
+	@Override
+	public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0, 0, 600, 600);
+        
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        g2.setColor(Color.black);
+        try {
+            fileInFace = new Scanner(new FileReader(fileInPathPerFrame));
+            while(fileInFace.hasNextLine()) {
+                int xc = fileInFace.nextInt();
+                int yc = fileInFace.nextInt();
+                int a = fileInFace.nextInt();
+                int b = fileInFace.nextInt();
+                midpointEllipse(g2, xc, yc, a, b);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        g2.setColor(Color.black);
+        try {
+            fileInClockNumber = new Scanner(new FileReader("src/halftone/xyPosition/xyOfClockNumber.txt"));
+            while(fileInClockNumber.hasNextLine()) {
+                int xc = fileInClockNumber.nextInt();
+                int yc = fileInClockNumber.nextInt();
+                int a = fileInClockNumber.nextInt();
+                fillMidpointCircle(g2, xc, yc, a);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        public CirclePane(int numberOfCircles) {
-            this.numberOfCircles = numberOfCircles;
-            
-            circles = new Circle[numberOfCircles];
-            
-            for (int i = 0; i < numberOfCircles; i++) {
-                Point coords = new Point((int) (Math.random() * 500) + 15, (int) (Math.random() * 450) + 15); //We generate random coords
-                int size = (int) (Math.random() * 50) + 25; //And random sizes
-                circles[i] = new Circle(coords, size); //Finally we create a new Circle with these properties
+        //--clockwise--
+        g2.rotate(rotate1, 300, 300);
+        
+        for (int i = 1; i < 35; i++) {      //small hand
+            fillMidpointCircle(g2, 300, (302 - (i * 2)), 4);
+        }
+        g2.rotate(rotate2, 300, 300);
+        
+        for (int i = 1; i < 90; i++) {      //minute hand
+            fillMidpointCircle(g2, 300, (302 - (i * 2)), 4);
+        }
+
+        fillMidpointCircle(g2, 300, 300, 6);
+        g2.setColor(Color.WHITE);
+        fillMidpointCircle(g2, 300, 300, 3);
+    }
+    public void midpointCircle(Graphics g, int xc, int yc, int r) {
+        int x = 0;
+        int y = r;
+        int d = 1 - r;
+        int dx = 2 * x;
+        int dy = 2 * y;
+
+        while(x <= y) {
+            plot(g, x + xc, y + yc, 1);
+            plot(g, -x + xc, y + yc, 1);
+            plot(g, x + xc, -y + yc, 1);
+            plot(g, -x + xc, -y + yc, 1);
+            plot(g, y + xc, x + yc, 1);
+            plot(g, -y + xc, x + yc, 1);
+            plot(g, y + xc, -x + yc, 1);
+            plot(g, -y + xc, -x + yc, 1);
+            x++;
+
+            dx += 2;
+
+            d = d + dx + 1;
+
+            if(d >= 0) {
+                y--;
+                dy -= 2;
+                d = d - dy;
+            }
+        }
+    }
+    public void midpointEllipse(Graphics g, int xc, int yc, int a, int b) { //a, b = radius
+        //region 1
+        int x, y, d;
+        x = 0;
+        y = b;
+        d = Math.round(b * b - a * a * b + a * a / 4);
+
+        while(b * b * x <= a * a * y) {
+            plot(g, x + xc, y + yc, 1);
+            plot(g, -x + xc, y + yc, 1);
+            plot(g, x + xc, -y + yc, 1);
+            plot(g, -x + xc, -y + yc, 1);
+
+            x++;
+            d = d + 2 * b * b * x + b * b;
+
+            if(d >= 0) {
+                y--;
+                d = d - 2 * a * a * y;
             }
         }
 
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
-            
-        }
+        //region 2
+        x = a;
+        y = 0;
+        d = Math.round(a * a - b * b * a + b * b / 4);
 
-        @Override
-        public void paintComponent(Graphics g) {
-            // super.paintComponent(g);     /* ******************** */
-            Graphics2D g2d = (Graphics2D) g;
-            
-            for (int i = 0; i < numberOfCircles; i++) {
-                g2d.draw(new Ellipse2D.Double(circles[i].getCoords().getX(), circles[i].getCoords().getY(), circles[i].getSize(), circles[i].getSize())); //We iterate over each circle in the array and paint it according to its coords and sizes
+        while(b * b * x >= a * a * y) {
+            plot(g, x + xc, y + yc, 1);
+            plot(g, -x + xc, y + yc, 1);
+            plot(g, x + xc, -y + yc, 1);
+            plot(g, -x + xc, -y + yc, 1);
+
+            y++;
+            d = d + 2 * a * a * y + a * a;
+
+            if(d >= 0) {
+                x--;
+                d = d - 2 * b * b * x;
             }
         }
     }
+    public void fillMidpointCircle(Graphics g, int xc, int yc, int r) {
+        for(int i = 0; i <= r; i++) {
+            midpointCircle(g, xc, yc, i);
+        }
+    }
+    public void plot(Graphics g, int x, int y, int size) {
+		g.fillRect(x, y, size, size);
+	}
 }
--- JavaProjectHalftone/src/FaceTest.java
+++ JavaProjectHalftone/src/FaceTest.java
@@ -1,208 +1,208 @@
-import java.util.Scanner;
-import java.awt.*;
-import javax.swing.*;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+// import java.util.Scanner;
+// import java.awt.*;
+// import javax.swing.*;
+// import java.io.FileReader;
+// import java.io.FileNotFoundException;
+// import java.io.IOException;
 
-public class FaceTest extends JPanel implements Runnable {
-    static Scanner fileInFace, fileInClockNumber;
-    static Scanner scn = new Scanner(System.in);
+// public class FaceTest extends JPanel implements Runnable {
+//     static Scanner fileInFace, fileInClockNumber;
+//     static Scanner scn = new Scanner(System.in);
     
-    double halftime = 0.0f;
-    double circleMove = 0.0f;
-    double squareRotate = 0.0f;
-    double rotate1 = 0.0f, rotate2 = 0.0f;
-    double x = 0, y = 550;
-    double velocity = 50;
-    double angle = -60;
+//     double halftime = 0.0f;
+//     double circleMove = 0.0f;
+//     double squareRotate = 0.0f;
+//     double rotate1 = 0.0f, rotate2 = 0.0f;
+//     double x = 0, y = 550;
+//     double velocity = 50;
+//     double angle = -60;
     
-    String fileInPathPerFrame;
+//     String fileInPathPerFrame;
 
-    public static void main(String[] args) throws IOException {
-		FaceTest m = new FaceTest();
+//     public static void main(String[] args) throws IOException {
+// 		FaceTest m = new FaceTest();
 
-		JFrame frame = new JFrame();
-		frame.add(m);
-		frame.setTitle("Animation Hell Yeah");
-		frame.setSize(600, 600);
-        frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+// 		JFrame frame = new JFrame();
+// 		frame.add(m);
+// 		frame.setTitle("Animation Hell Yeah");
+// 		frame.setSize(600, 600);
+//         frame.setLocationRelativeTo(null);
+// 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// 		frame.setVisible(true);
 
-        (new Thread(m)).start();
-	}
-    public void run() {
-        int numberFileIn = 0;
-        double currentTime, elapsedTime, keyFrameTime = 0;
-        double lastTime = System.currentTimeMillis();
+//         (new Thread(m)).start();
+// 	}
+//     public void run() {
+//         int numberFileIn = 0;
+//         double currentTime, elapsedTime, keyFrameTime = 0;
+//         double lastTime = System.currentTimeMillis();
 
-        while(true) {
-            //control time
-            currentTime = System.currentTimeMillis();
-            elapsedTime = currentTime - lastTime;
-            lastTime = currentTime;
+//         while(true) {
+//             //control time
+//             currentTime = System.currentTimeMillis();
+//             elapsedTime = currentTime - lastTime;
+//             lastTime = currentTime;
             
-            //update
-            rotate1 += 0.1 * elapsedTime / 1000.0;
-            rotate2 += 1.125 * elapsedTime / 1000.0;
-            // ^---- keyFrameTime % 2.5 == 0
-            /* rotate1 += 0.10 * elapsedTime / 1000.0; 
-            rotate2 += 1.2 * elapsedTime / 1000.0; */
+//             //update
+//             rotate1 += 0.1 * elapsedTime / 1000.0;
+//             rotate2 += 1.127 * elapsedTime / 1000.0;
+//             // ^---- keyFrameTime % 2.5 == 0
+//             /* rotate1 += 0.10 * elapsedTime / 1000.0; 
+//             rotate2 += 1.2 * elapsedTime / 1000.0; */
 
-            keyFrameTime += elapsedTime / 1000.0;
+//             keyFrameTime += elapsedTime / 1000.0;
             
-            if(keyFrameTime == 0.0f) {
-                fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage" + numberFileIn + ".txt";
-                halftime += 0.625f;
-                numberFileIn++;
-                System.out.printf("Assignment2 ver %.1f\n", keyFrameTime);
-            } else if(keyFrameTime >= halftime) {
-                if(numberFileIn <= 40) {
-                    fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage" + numberFileIn + ".txt";
-                    // System.out.println(numberFileIn);
-                    halftime += 0.625f;
-                    numberFileIn++;
-                } else {
-                    fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage40.txt";
-                    System.out.printf("Za Warudo!!!\nThread Killed!\n");
+//             if(keyFrameTime == 0.0f) {
+//                 fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage" + numberFileIn + ".txt";
+//                 halftime += 0.625f;
+//                 numberFileIn++;
+//                 System.out.printf("Assignment2 ver %.1f\n", keyFrameTime);
+//             } else if(keyFrameTime >= halftime) {
+//                 if(numberFileIn <= 40) {
+//                     fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage" + numberFileIn + ".txt";
+//                     // System.out.println(numberFileIn);
+//                     halftime += 0.625f;
+//                     numberFileIn++;
+//                 } else {
+//                     fileInPathPerFrame = "src/halftone/xyPosition/xyOfImage40.txt";
+//                     System.out.printf("Za Warudo!!!\nThread Killed!\n");
 
-                    //Stop Thread
-                    return; 
-                }
-            }
+//                     //Stop Thread
+//                     return;
+//                 }
+//             }
 
-            //display
-            repaint();
-        }
-    }
-	@Override
-	public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.WHITE);
-        g2.fillRect(0, 0, 600, 600);
+//             //display
+//             repaint();
+//         }
+//     }
+// 	@Override
+// 	public void paintComponent(Graphics g) {
+//         Graphics2D g2 = (Graphics2D)g;
+//         g2.setColor(Color.WHITE);
+//         g2.fillRect(0, 0, 600, 600);
         
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
-        g2.setColor(Color.black);
-        try {
-            fileInFace = new Scanner(new FileReader(fileInPathPerFrame));
-            while(fileInFace.hasNextLine()) {
-                int xc = fileInFace.nextInt();
-                int yc = fileInFace.nextInt();
-                int a = fileInFace.nextInt();
-                int b = fileInFace.nextInt();
-                midpointEllipse(g2, xc, yc, a, b);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        g2.setColor(Color.black);
-        try {
-            fileInClockNumber = new Scanner(new FileReader("src/halftone/xyPosition/xyOfClockNumber.txt"));
-            while(fileInClockNumber.hasNextLine()) {
-                int xc = fileInClockNumber.nextInt();
-                int yc = fileInClockNumber.nextInt();
-                int a = fileInClockNumber.nextInt();
-                fillMidpointCircle(g2, xc, yc, a);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//         g2.setColor(Color.black);
+//         try {
+//             fileInFace = new Scanner(new FileReader(fileInPathPerFrame));
+//             while(fileInFace.hasNextLine()) {
+//                 int xc = fileInFace.nextInt();
+//                 int yc = fileInFace.nextInt();
+//                 int a = fileInFace.nextInt();
+//                 int b = fileInFace.nextInt();
+//                 midpointEllipse(g2, xc, yc, a, b);
+//             }
+//         } catch (FileNotFoundException e) {
+//             e.printStackTrace();
+//         }
+//         g2.setColor(Color.black);
+//         try {
+//             fileInClockNumber = new Scanner(new FileReader("src/halftone/xyPosition/xyOfClockNumber.txt"));
+//             while(fileInClockNumber.hasNextLine()) {
+//                 int xc = fileInClockNumber.nextInt();
+//                 int yc = fileInClockNumber.nextInt();
+//                 int a = fileInClockNumber.nextInt();
+//                 fillMidpointCircle(g2, xc, yc, a);
+//             }
+//         } catch (FileNotFoundException e) {
+//             e.printStackTrace();
+//         }
 
-        //--clockwise--
-        g2.rotate(rotate1, 300, 300);
+//         //--clockwise--
+//         g2.rotate(rotate1, 300, 300);
         
-        for (int i = 1; i < 35; i++) {      //small hand
-            fillMidpointCircle(g2, 300, (302 - (i * 2)), 4);
-        }
-        g2.rotate(rotate2, 300, 300);
+//         for (int i = 1; i < 35; i++) {      //small hand
+//             fillMidpointCircle(g2, 300, (302 - (i * 2)), 4);
+//         }
+//         g2.rotate(rotate2, 300, 300);
         
-        for (int i = 1; i < 90; i++) {      //minute hand
-            fillMidpointCircle(g2, 300, (302 - (i * 2)), 4);
-        }
+//         for (int i = 1; i < 90; i++) {      //minute hand
+//             fillMidpointCircle(g2, 300, (302 - (i * 2)), 4);
+//         }
 
-        fillMidpointCircle(g2, 300, 300, 6);
-        g2.setColor(Color.WHITE);
-        fillMidpointCircle(g2, 300, 300, 3);
-    }
-    public void midpointCircle(Graphics g, int xc, int yc, int r) {
-        int x = 0;
-        int y = r;
-        int d = 1 - r;
-        int dx = 2 * x;
-        int dy = 2 * y;
+//         fillMidpointCircle(g2, 300, 300, 6);
+//         g2.setColor(Color.WHITE);
+//         fillMidpointCircle(g2, 300, 300, 3);
+//     }
+//     public void midpointCircle(Graphics g, int xc, int yc, int r) {
+//         int x = 0;
+//         int y = r;
+//         int d = 1 - r;
+//         int dx = 2 * x;
+//         int dy = 2 * y;
 
-        while(x <= y) {
-            plot(g, x + xc, y + yc, 1);
-            plot(g, -x + xc, y + yc, 1);
-            plot(g, x + xc, -y + yc, 1);
-            plot(g, -x + xc, -y + yc, 1);
-            plot(g, y + xc, x + yc, 1);
-            plot(g, -y + xc, x + yc, 1);
-            plot(g, y + xc, -x + yc, 1);
-            plot(g, -y + xc, -x + yc, 1);
-            x++;
+//         while(x <= y) {
+//             plot(g, x + xc, y + yc, 1);
+//             plot(g, -x + xc, y + yc, 1);
+//             plot(g, x + xc, -y + yc, 1);
+//             plot(g, -x + xc, -y + yc, 1);
+//             plot(g, y + xc, x + yc, 1);
+//             plot(g, -y + xc, x + yc, 1);
+//             plot(g, y + xc, -x + yc, 1);
+//             plot(g, -y + xc, -x + yc, 1);
+//             x++;
 
-            dx += 2;
+//             dx += 2;
 
-            d = d + dx + 1;
+//             d = d + dx + 1;
 
-            if(d >= 0) {
-                y--;
-                dy -= 2;
-                d = d - dy;
-            }
-        }
-    }
-    public void midpointEllipse(Graphics g, int xc, int yc, int a, int b) { //a, b = radius
-        //region 1
-        int x, y, d;
-        x = 0;
-        y = b;
-        d = Math.round(b * b - a * a * b + a * a / 4);
+//             if(d >= 0) {
+//                 y--;
+//                 dy -= 2;
+//                 d = d - dy;
+//             }
+//         }
+//     }
+//     public void midpointEllipse(Graphics g, int xc, int yc, int a, int b) { //a, b = radius
+//         //region 1
+//         int x, y, d;
+//         x = 0;
+//         y = b;
+//         d = Math.round(b * b - a * a * b + a * a / 4);
 
-        while(b * b * x <= a * a * y) {
-            plot(g, x + xc, y + yc, 1);
-            plot(g, -x + xc, y + yc, 1);
-            plot(g, x + xc, -y + yc, 1);
-            plot(g, -x + xc, -y + yc, 1);
+//         while(b * b * x <= a * a * y) {
+//             plot(g, x + xc, y + yc, 1);
+//             plot(g, -x + xc, y + yc, 1);
+//             plot(g, x + xc, -y + yc, 1);
+//             plot(g, -x + xc, -y + yc, 1);
 
-            x++;
-            d = d + 2 * b * b * x + b * b;
+//             x++;
+//             d = d + 2 * b * b * x + b * b;
 
-            if(d >= 0) {
-                y--;
-                d = d - 2 * a * a * y;
-            }
-        }
+//             if(d >= 0) {
+//                 y--;
+//                 d = d - 2 * a * a * y;
+//             }
+//         }
 
-        //region 2
-        x = a;
-        y = 0;
-        d = Math.round(a * a - b * b * a + b * b / 4);
+//         //region 2
+//         x = a;
+//         y = 0;
+//         d = Math.round(a * a - b * b * a + b * b / 4);
 
-        while(b * b * x >= a * a * y) {
-            plot(g, x + xc, y + yc, 1);
-            plot(g, -x + xc, y + yc, 1);
-            plot(g, x + xc, -y + yc, 1);
-            plot(g, -x + xc, -y + yc, 1);
+//         while(b * b * x >= a * a * y) {
+//             plot(g, x + xc, y + yc, 1);
+//             plot(g, -x + xc, y + yc, 1);
+//             plot(g, x + xc, -y + yc, 1);
+//             plot(g, -x + xc, -y + yc, 1);
 
-            y++;
-            d = d + 2 * a * a * y + a * a;
+//             y++;
+//             d = d + 2 * a * a * y + a * a;
 
-            if(d >= 0) {
-                x--;
-                d = d - 2 * b * b * x;
-            }
-        }
-    }
-    public void fillMidpointCircle(Graphics g, int xc, int yc, int r) {
-        for(int i = 0; i <= r; i++) {
-            midpointCircle(g, xc, yc, i);
-        }
-    }
-    public void plot(Graphics g, int x, int y, int size) {
-		g.fillRect(x, y, size, size);
-	}
-}
+//             if(d >= 0) {
+//                 x--;
+//                 d = d - 2 * b * b * x;
+//             }
+//         }
+//     }
+//     public void fillMidpointCircle(Graphics g, int xc, int yc, int r) {
+//         for(int i = 0; i <= r; i++) {
+//             midpointCircle(g, xc, yc, i);
+//         }
+//     }
+//     public void plot(Graphics g, int x, int y, int size) {
+// 		g.fillRect(x, y, size, size);
+// 	}
+// }
